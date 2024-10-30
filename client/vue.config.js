9302f27ca7df3587b20d3ffe106b2f24dd8c107b
Azaken1248 <suvkrm@gmail.com>
2024-10-31 03:06:41 +0530

    Build Website

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  	base: '/HouseholdVueApp/',
-	transpileDependencies: true
+    publicPath: '/HouseholdVueApp/',
+    transpileDependencies: true
 })
