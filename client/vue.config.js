2fe28757aed578d2d311a91ed6e40f445c1c0441
Azaken1248 <suvkrm@gmail.com>
2024-11-15 02:23:50 +0530

    Added new path

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
-    publicPath: '/HouseholdVueApp/',
+    publicPath: './',
     transpileDependencies: true
 })
